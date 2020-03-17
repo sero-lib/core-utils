af87de692cb931061c1be9e1478dddb8b0575bf7
Yuhanun <y.citgez@student.utwente.nl>
2020-03-17 18:37:49 +0100

    Now using travis for this repo

---
+++ docker/compile_and_run.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+pacman --noconfirm -Syu
+
+cd /opt/repo || exit 1
+git pull
+git submodule foreach git pull
+
+mkdir build && cd build || exit 1
+cmake .. \
+    -DCMAKE_CXX_COMPILER=/usr/lib/ccache/bin/clang++ \
+    -DCMAKE_CC_COMPILER=/usr/lib/ccache/bin/clang
+cmake --build .
+
+./sero_core_utils_tests
