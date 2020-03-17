32d6d200dafe9ce00a764ca39289766bf2290935
Yuhanun <y.citgez@student.utwente.nl>
2020-03-17 18:47:04 +0100

    Updated docker files

--- docker/compile_and_run.sh
+++ docker/compile_and_run.sh
@@ -11,7 +11,8 @@ git submodule foreach git pull
 mkdir build && cd build || exit 1
 cmake .. \
     -DCMAKE_CXX_COMPILER=/usr/lib/ccache/bin/clang++ \
-    -DCMAKE_CC_COMPILER=/usr/lib/ccache/bin/clang
+    -DCMAKE_CC_COMPILER=/usr/lib/ccache/bin/clang \
+    -GNinja
 cmake --build .
 
 ./sero_core_utils_tests
